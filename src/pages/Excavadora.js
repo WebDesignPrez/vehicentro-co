d027a6bda29d3ea070a280f983a35b2ce9c338fb
WebDesignPrez <web.design.prez@gmail.com>
2022-12-16 15:43:28 -0500

    link 18t

--- src/pages/Excavadora.js
+++ src/pages/Excavadora.js
@@ -60,12 +60,91 @@ function Camion() {
             textoMotor1 = "SUNWARD"
             textoMotor2 = "Swe210"
             textoMotor3 = "Escavadora"
-            precio = "Contáctanos"
+            precio = "$161.280"
             motor = "../images/swe210/motor.webp"
             garantia = "../images/swe210/5-anos-de-garantia.webp"
             tecnologia = "../images/swe210/sunward.webp"
             contenedorVidaUtil = <div className="banner"><img src="../images/swe210/isuzu.webp" /></div>
-            
+
+            caracteristicas = [
+                "../images/swe210/camion-de-24-toneladas-potencia-340hp.webp",
+                "../images/swe210/camion-de-24-toneladas-cilindraje.webp",
+                "../images/swe210/camion-de-24-toneladas-freno-a-las-valvulas.webp",
+                "../images/swe210/inyeccion.webp"
+            ]
+            cabina = "../images/swe210/cabina.webp"
+            marcas = "../images/swe210/asiento.webp"
+            internas1 = [
+                "../images/swe210/motor-giro.webp",
+                "../images/swe210/sunroof.webp"
+            ];
+            internas2 = [
+                "../images/swe210/cluster.webp",
+                "../images/swe210/asiento.webp"
+            ];
+            internas1b = [
+                "../images/swe210/bomba.webp"
+            ];
+            internas2b = [
+                "../images/swe210/radio.webp"
+            ];
+
+            medidas = "../images/swe210/medidas.webp"
+            medidasDos = "../images/swe210/medidas-2.webp"
+            medidasMovil = "../images/swe210/medidas-movil.webp"
+            medidasMovilDos = "../images/swe210/medidas-movil-2.webp"
+            audio_motor = "../images/swe210/motor.wav"
+
+            internas = ((internas1.concat(internas2)).concat(internas1b)).concat(internas2b)
+            //loop slider
+
+            slide3 = internas1b.map((interna) =>
+                <div className="each-slide-effect" >
+                    <div style={{ 'backgroundImage': `url(${interna})`, height: `25vw` }}>
+                    </div>
+                </div>
+            );
+
+            slide3b = internas2b.map((interna) =>
+                <div className="each-slide-effect" >
+                    <div style={{ 'backgroundImage': `url(${interna})`, height: `25vw` }}>
+                    </div>
+                </div>
+            );
+
+            contenedorMarcas = <div style={{ display: `contents` }} >
+                <div className="boxRightHeader  half noResponsive" style={{ width: `50%` }}>
+                    <Slide >
+                        {slide3}
+                    </Slide>
+                </div>
+                <div className="boxRightHeader  half noResponsive" style={{ width: `50%` }}>
+                    <Slide >
+                        {slide3b}
+                    </Slide>
+                </div>
+            </div>
+
+            break;
+
+        case "swe365e":
+            console.log('24 ton')
+            nombreCamion = "Excavadora SWE365E"
+            camionSerie = "VOLQUETA T5G 430-330 10-14M2"
+            nombreSerie = "Especiales"
+            bdc = "https://bdc.vehicentro.com:9443/ords/ws_vehicentro/api/conexiones/wordpress/WEB24TT5G4187"
+            images = [
+                "../images/swe210/vehicentro-excavadora-ecuador.webp"
+            ];
+            textoMotor1 = "SUNWARD"
+            textoMotor2 = "swe365e"
+            textoMotor3 = "Escavadora"
+            precio = "$161.280"
+            motor = "../images/swe210/motor.webp"
+            garantia = "../images/swe210/5-anos-de-garantia.webp"
+            tecnologia = "../images/swe210/sunward.webp"
+            contenedorVidaUtil = <div className="banner"><img src="../images/swe210/isuzu.webp" /></div>
+
             caracteristicas = [
                 "../images/swe210/camion-de-24-toneladas-potencia-340hp.webp",
                 "../images/swe210/camion-de-24-toneladas-cilindraje.webp",
@@ -132,7 +211,7 @@ function Camion() {
     }
 
 
-    
+
 
     //loop primer slide
     let slide1 = images.map((image) =>
@@ -189,7 +268,9 @@ function Camion() {
             <div className="marcasbg textoImagenesProducto" >
 
                 <div className="boxRightHeader  half text_icon_left" >
+                    <p className='vidaUtil'>DESDE</p>
                     <p className='precioExcavadora' >{precio}</p>
+                    <p className='cuotas'>Cuotas desde: $123</p>
                 </div>
 
                 <div className="boxRightHeader  half icon_center" >
