dce21a15265c985cfa7dd79a574d90cf3ce1fdec
Marco Montero <marcomonterin@gmail.com>
2022-12-16 13:39:45 -0500

    Ventana emergente banner contacto

--- src/components/BannerContacto.js
+++ src/components/BannerContacto.js
@@ -1,12 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import FormContact from "./FormContact";
 import Modal from 'react-modal';
+import '../modalContact.css';
 
 export default function BannerContacto(){
 
   Modal.setAppElement('#root');
   const [modalIsOpen, setIsOpen] = useState(false);
-
+  const [modalContactIsOpen, setContactIsOpen] = useState(false);
 
   const customStyles = {
       content: {
@@ -20,9 +21,23 @@ export default function BannerContacto(){
       },
     };
 
+    useEffect(() => {
+      const keyDownHandler = event => {
+        if (event.key === 'Escape') {
+          event.preventDefault();
+          closeContact()
+        }
+      };
+  
+      document.addEventListener('keydown', keyDownHandler);
+      return () => {
+        document.removeEventListener('keydown', keyDownHandler);
+      };
+    }, []);
 
   function openModal() {
       setIsOpen(true);
+      closeContact()
     }
   
     function afterOpenModal() {
@@ -34,13 +49,41 @@ export default function BannerContacto(){
       setIsOpen(false);
     }
 
+    function openContact(){
+      setContactIsOpen(true)
+    }
+
+
+    function closeContact(){
+      setContactIsOpen(false)
+    }
+
+
+    function gotoWs(){
+      closeContact()
+    }
+
+    function gotoCall(){
+      closeContact()
+    }
 
   return (
     <>
           <div className="boxesContact posRelative">
-              <p className="buttonContact" onClick={openModal}>QUIERO SER CONTACTADO AHORA</p>
+              <p className="buttonContact" onClick={openContact}>QUIERO SER CONTACTADO AHORA</p>
               <p className="textContact">Dejanos tus datos y uno de nuestros asesores se contactara contigo</p>
           </div>
+
+          {modalContactIsOpen &&
+          <div className="modalContact" id="modalBanner" >
+            <div className="headerModalContact"><img src="./images/vehicentro-logo-blanco.png" alt="Vehicentro" /></div>
+            <div className="bodyModalContact">
+              <div className="btnModalContact" onClick={gotoWs}><span className="wa">ESCRIBENOS</span></div>
+              <div className="btnModalContact" onClick={gotoCall}><span className="ca">LLAMADA DIRECTA</span></div>
+              <div className="btnModalContact" onClick={openModal}><span>DEJANOS TUS DATOS</span></div>
+            </div>
+          </div>
+}
         
       <Modal
           isOpen={modalIsOpen}
