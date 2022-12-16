1cae3c8ff522b975667a27a095c36254e04aac46
WebDesignPrez <web.design.prez@gmail.com>
2022-12-16 15:31:46 -0500

    link 18t

--- src/NavBarTop.js
+++ src/NavBarTop.js
@@ -277,8 +277,8 @@ export default function NavBarTop() {
                             <p className="colItem meniu"><NavLink to="/camiones/camion-12-toneladas-t5g-1167">T5G 1167 / 12 ton</NavLink></p>
                             <p className="colItem meniu"><NavLink to="/camiones/camion-13-toneladas-t5g-1167">T5G 1167 / 13 ton (280hp)</NavLink></p>
                             <p className="colItem meniu"><NavLink to="/camiones/camion-13-toneladas-t5g-1167-330hp">T5G 1167 / 13 ton (330hp)</NavLink></p>
-                            <p className="colItem meniu"><NavLink to="/camiones/camion-de-19-toneladas-t5g-1257">T5G 1167 / 18 ton</NavLink></p>
-                            <p className="colItem meniu"><NavLink to="/camiones/camion-de-18-toneladas-t5g-1257">T5G 1167 / 19.5 ton</NavLink></p>
+                            <p className="colItem meniu"><NavLink to="/camiones/camion-de-18-toneladas-t5g-1257">T5G 1167 / 18 ton</NavLink></p>
+                            <p className="colItem meniu"><NavLink to="/camiones/camion-de-19-toneladas-t5g-1257">T5G 1167 / 19.5 ton</NavLink></p>
                             <p className="colItem meniu"><NavLink to="/cabezales/cabezal-t5g-4187">T5G 1167 / 24 ton</NavLink></p>
 
                         </div>
