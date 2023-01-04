4021af3989d23c811a30b59186a4da83ec17f8b9
WebDesignPrez <web.design.prez@gmail.com>
2023-01-04 12:47:27 -0500

    cambios 3

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -18,6 +18,7 @@ export default function Footer(){
                     <a href="https://www.facebook.com/Vehicentro"><img src="../images/facebook.png"  alt="facebook" width="32" height="32" /></a>
                     <a href="https://www.instagram.com/vehicentroec"><img src="../images/instagram.png"  alt="instagram" width="32" height="32" /></a>
                     <a href="https://www.youtube.com/channel/UCW8gGawovE35cXcJ3P2kryQ"><img src="../images/youtube.png"  alt="youtube" width="32" height="32" /></a>
+                    <a href="https://www.tiktok.com/@sinotrukeros"><img src="../images/tiktok.webp"  alt="tiktok" width="32" height="32" /></a>
                     <a href="#"><img src="../images/linkdin.png"  alt="linkdin" width="32" height="32" /></a>          
                   </div>
                   
