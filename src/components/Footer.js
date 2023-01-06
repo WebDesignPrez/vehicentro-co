c433fadabac9a1122d79f0b69bd6a8b0fb0b3a4f
WebDesignPrez <web.design.prez@gmail.com>
2023-01-06 10:40:40 -0500

    cambios 18

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom"
-export default function Footer(){
+export default function Footer() {
     return (
-    <div className="footer posRelative">
+        <div className="footer posRelative">
             <div className="info">
-            <img src="../images/vehicentro-logo-blanco.png" width="230" height="80" alt="Vehicentro" />
-                 <ul className="list">
+                <img src="../images/vehicentro-logo-blanco.png" width="230" height="80" alt="Vehicentro" />
+                <ul className="list">
                     <li><p className="titleFooter"><a href="/compania">NOSOTROS</a></p></li>
                     <li><p className="titleFooter"><a href="/concesionarios">CONCESIONARIOS</a></p></li>
                     <li><p>VEINSTEIN</p></li>
@@ -14,14 +14,14 @@ export default function Footer(){
                 <p className="title titleNumero"><a href="tel:032294740">032 994 740</a></p>
                 <p className="title titleContacto">CONTACTO DIRECTO</p>
                 <div className="social row">
-                  <div className="flex-row imgMain">
-                    <a href="https://www.facebook.com/Vehicentro"><img src="../images/facebook.png"  alt="facebook" width="32" height="32" /></a>
-                    <a href="https://www.instagram.com/vehicentroec"><img src="../images/instagram.png"  alt="instagram" width="32" height="32" /></a>
-                    <a href="https://www.youtube.com/channel/UCW8gGawovE35cXcJ3P2kryQ"><img src="../images/youtube.png"  alt="youtube" width="32" height="32" /></a>
-                    <a href="https://www.tiktok.com/@sinotrukeros"><img src="../images/tiktok.webp"  alt="tiktok" width="32" height="32" /></a>
-                    <a href="#"><img src="../images/linkdin.png"  alt="linkdin" width="32" height="32" /></a>          
-                  </div>
-                  
+                    <div className="flex-row imgMain">
+                        <a href="https://www.facebook.com/Vehicentro"><img src="../images/facebook.png" alt="facebook" width="32" height="32" /></a>
+                        <a href="https://www.instagram.com/vehicentroec"><img src="../images/instagram.png" alt="instagram" width="32" height="32" /></a>
+                        <a href="https://www.youtube.com/channel/UCW8gGawovE35cXcJ3P2kryQ"><img src="../images/youtube.png" alt="youtube" width="32" height="32" /></a>
+                        <a href="https://www.tiktok.com/@sinotrukeros"><img src="../images/tiktok.webp" alt="tiktok" width="32" height="32" /></a>
+                        <a href="#"><img src="../images/linkdin.png" alt="linkdin" width="32" height="32" /></a>
+                    </div>
+
                 </div>
             </div>
             <div className="col1 colProd">
@@ -36,10 +36,10 @@ export default function Footer(){
                 </ul>
                 <ul className="list">
                     <li><p className="subTitle">SUNWARD</p></li>
-                    <li><p  className="titleFooter"><a href="/excavadoras/swe210">Excavadora SWE210</a></p></li>
-                    <li><p  className="titleFooter"><a href="/excavadoras/swe365e">Excavadora SWE365E</a></p></li>
+                    <li><p className="titleFooter"><a href="/excavadoras/swe210">Excavadora SWE210</a></p></li>
+                    <li><p className="titleFooter"><a href="/excavadoras/swe365e">Excavadora SWE365E</a></p></li>
                 </ul>
-                
+
             </div>
             <div className="col1 colSell">
                 <p className="title">POST VENTA</p>
@@ -56,10 +56,11 @@ export default function Footer(){
                     <li><p className="itemMenu">Componentes</p></li>
                     <li><p className="itemMenu">Medición de rendimiento</p></li>
                     <li><a href="https://bdc.vehicentro.com:9443/ords/ws_vehicentro/r/crm/login"><p className="itemMenu">BDC</p></a></li>
-                    
-                    
-                    
                 </ul>
+
+                <a href="https://api.whatsapp.com/send?phone=593988593279" class="btn-wsp" target="_blank">
+                    <img className="imgWpp" src="../images/wpp.gif"></img>
+                </a>
             </div>
         </div>
     )
