11ea055e3a4c6bb46119b58fa64fc9df7187a008
WebDesignPrez <web.design.prez@gmail.com>
2023-01-24 15:40:22 -0500

    cambios 69

---
+++ src/pages/Proximamente-hidromek.js
@@ -0,0 +1,24 @@
+import BannerContacto from "../components/BannerContacto"
+import Footer from "../components/Footer"
+import NavBarTop from "../NavBarTop"
+
+function Proximamente() {
+    return (
+        <>
+            <NavBarTop />
+            <Content1 />
+            <BannerContacto />
+            <Footer />
+        </>
+    )
+}
+
+function Content1() {
+    return (
+        <div className="posRelative banner">
+            <img className=" slideMain" src="./images/proximamente.webp" alt="Punto de Venta" />
+        </div>
+    )
+}
+
+export default Proximamente
